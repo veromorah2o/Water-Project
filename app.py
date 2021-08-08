87c4edd1337e52e9a9046f6e83aa4e85dc88f80d
tengmorgan <88216635+tengmorgan@users.noreply.github.com>
2021-08-08 15:01:14 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,160 +1,148 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # 2D Contamination Spread in Groundwater
-# 
-# 
+print('1D Contamination Spread in Groundwater')
+# -pollution 
+# -spread,movement, transport,distribution
+# -define porosity & absorption(interaction,association w/ soil or rock), chemical interaction with the aquifer 
+# -story telling! 
+# -explain the model and plot and what we would see in the plot 
 # Problem:<br>
-# An air force base was found to be releasing sewage into groundwater! The spill of the sewage continues to happen without stopping! In order to protect the local residents near the air force base, we need to know where the groundwater has been contaminated and stop local residents from using groundwater. The following program can help you check which locations contain polluted water and we can stop it from being used! 
+# On a hot summer day, lab assistant Sally was given a task to disposal a bottle of chemical. Without much thinking, she pours the chemical right down the drain! It was later found that the chemical being poured is benzene and spills directly into the groundwater! In order to prevent the local residents using the contaminated water, use the following contamination model to check the spread of the benzene. 
 # 
 # Model Application:<br> 
-# Look at the map, the sewage spill happens at the red dot location. Use the three built-in variables to check how the sewage spreads. Use the time variable to check the spread pattern as time passes after the start of the spill. The velocity variable represents how fast the groundwater is moving towards the right on the map. The dispersion factor is a soil property showing which direction the sewage would spread. Adjust the three variables and check how the spread pattern of the sewage changes over time! 
-# 
+# The spill occured at location where distance is 0 meter. The following program shows the concentration vs location curves at three different times after the spill occurance: 5, 10, and 20 days. (The groundwater in the aquifer has a flow speed of 1.667 meter/day in the positive horizontal direction.) You can adjust the mass of the spill, the porosity, and absorption of the aquifer in order to check concentrations at different locations under various spill mass and geological features. Try to change all three varibales built into the program and see their effects on the concentrations at different locations.
+# -explain what the users will see in the plot
+# -line vs area
+# -visual aids 
+# -choices for porosity 
+# -delete absorption (R=1)
 # Questions: <br>
-# 1) What happens to the shape of the contaminant spreading as the dispersion factor increases?<br>
-# 2) If time and dispersion factor stay constant, what happens to the contaminant spreading as the velocity changes?<br>
-# 3) Observe and describe the shape of the contaminant spreading as times goes on. Does the spreading pattern stop changing after a specific period of time?
-# 
+# 1) What is the effect on the maximum concentration when porosity increases? <br>
+# 2) If the pollution mass, porosity, and absorption all stay constant, what happens to the concentration as time goes on?<br>
+# 3) Don't change the pollution mass and porosity, observe and describe the changes to the curves as absoption changes?<br>
 # Advanced Questions:<br>
-# 1) The local city policy sets the limit of concentration at 3 kg/m^2 before the underground water can threaten the health of local residents. With a flow velocity of 2.1 meter/day, dispersion factor of 0.1, and aquifer thickness of 3 meters, would the residents live near (x=60000,y=0) be able to use underground water safely after 6 years?  <br>
-# 2) Knowing the flow velocity is 1.1 meter/day, dispersion fraction is 0.05, and aquifer thickness is 3 meters, after how much time the concentration would reach a steady state if no other variables are considered? <br>
-# *Steady State refers to the status where the concentrations are not changing anymore in the area of interest.
-# 
+# 1) A spill occured into an aquifer near UC Davis! The spill mass is known to have a mass of 800 kg, and soil test shows that the porosity of the aquifer is 0.36 and the absorption is 2.5. How far away horizontally can we expect the maximum concentration to occure after 5 days? and 20 days? <br>
+# 2) The city policy of Davis allows 20 kg/m of concentration for this contaminant. Knowing the porosity and absorption is 0.34 and 4.35 respectively. What is the maximum spill mass allowed in the aquifer) before violating the local environemtnal policy? 
 
 # In[1]:
 
 
-#import requried libraries and Bokeh functions 
-from bokeh.io import output_notebook 
-import numpy as np
-
-import math 
-import matplotlib.pyplot as plt 
-import matplotlib as mpl
-import scipy.special as s   
-import scipy.integrate as integrate
-from math import pi
-from bokeh.io import output_file, show
-from bokeh.models import BasicTicker, ColorBar, LinearColorMapper, ColumnDataSource, PrintfTickFormatter
-from bokeh.plotting import figure
-from bokeh.transform import transform
-from bokeh.application import Application
-from bokeh.application.handlers import FunctionHandler
-from bokeh.palettes import Viridis256
-import sys
-np.set_printoptions(threshold=np.inf)
-pd.options.display.max_seq_items = 2000
-import timeit
-start = timeit.timeit()
+import math
 import bokeh.plotting.figure as bk_figure
 from bokeh.io import curdoc, show
 from bokeh.layouts import row, widgetbox
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, Select
 from bokeh.models.widgets import Slider, TextInput
+import numpy as np
+from bokeh.layouts import column, row
 
-from bokeh.application import Application
-from bokeh.application.handlers import FunctionHandler
-output_file('2D_Bokeh.html')
-output_notebook()
-# Set initial data
-v= 2.55
-a = 0.1 
-time = 20
-
-# calculate the Z data values and return a dataframe
-def calculate_z(v, a, time):
-    x = np.linspace(2,10000,100)
-    y = np.linspace(-2000,2000,100)
-    C = np.zeros([len(x),len(y)])
-    for i in range(0,len(x)):
-        for j in range(0,len(y)):
-            b = 20
-            t = time*365
-            DL = v*0.83*(math.log10(x[i]))**2.414
-            Dt = a * DL
-            def f(r):
-                return math.exp((-((x[i]-(v*r))**2)/(4*DL*r))-(((y[j])**2)/(4*Dt*r)))*(1/r)
-            results,err = integrate.quad(f,0,t)
-            C[i,j] =((5*2*10**8/b)/(4*math.pi*((DL*Dt)**0.5)))*results
-    CC = np.transpose(C)
-    CCC = np.clip(CC,0,5)
-
-    df = pd.DataFrame(
-    CCC,
-    columns=x,
-    index=y)
-    df.columns.name = 'x'
-    df.index.name = 'y'
-    df = df.stack().rename("value").reset_index()
-
-    return x, y, df
-
-# calculate the dataframe
-x, y, df = calculate_z(v, a, time)
-source = ColumnDataSource(data=dict(df))
-#colormap =cm.get_cmap("BuPu")
-#bokehpalette = [mpl.colors.rgb2hex(m) for m in colormap(np.arange(colormap.N))]
+"""
+Initialize global variables and functions
+"""
+M = 500
+n = 0.375
+R = 1
+time1 = 5
+time2 = 10
+time3 = 20
 
 
-colors = ['#d7191c', '#fdae61', '#ffffbf', '#a6d96a', '#1a9641']
-mapper = LinearColorMapper(palette=Viridis256, low=df.value.min(), high=df.value.max())
- 
-p = figure(title="Concentration Signals (2 Dimension)")
-p.rect(x="x", y="y",width=(max(x)-min(x))/len(x),height=(max(y)-min(y))/len(y),source=source,fill_color=transform('value', mapper),line_color= None)
-color_bar = ColorBar(color_mapper=mapper,location=(0, 0),ticker=BasicTicker(desired_num_ticks=10))
-#color_bar.set_label('Concentration')
-p.xaxis.axis_label = 'Horizontal Distance (m)'
-p.yaxis.axis_label = 'Vertical Distance (m)'
-p.add_layout(color_bar, 'right')
+def calculate_z(M, n):
+    """
+    Calculates z values for use in Bokeh plot curves
+    :param M: mass (kg)
+    :param n: porosity
+    :return: x (range of curves), y/yy/y2 (concentration at different times)
+    """
+    x = np.arange(1.1, 400, 0.5)
+    y = np.zeros([len(x)])
+    yy = np.zeros([len(x)])
+    y2 = np.zeros([len(x)])
+    q = 1.667  # flow speed of groundwater in aquifer
+    v = q / n
+    D = v * 0.83 * (np.log10(x)) ** 2.414
+    y = (M / (4 * math.pi * time1 * np.sqrt(D / R))) * np.exp(
+        -((x - (((1.667 / n) * time1) / R)) ** 2) / (4 * D * time1 / R))
+    yy = (M / (4 * math.pi * time2 * np.sqrt(D / R))) * np.exp(
+        -((x - (((1.667 / n) * time2) / R)) ** 2) / (4 * D * time2 / R))
+    y2 = (M / (4 * math.pi * time3 * np.sqrt(D / R))) * np.exp(
+        -((x - (((1.667 / n) * time3) / R)) ** 2) / (4 * D * time3 / R))
+    return x, y, yy, y2
+
+
+x, y, yy, y2 = calculate_z(M, n)
+source = ColumnDataSource(data=dict(x=x, y=y))
+source1 = ColumnDataSource(data=dict(x=x, y=yy))
+source2 = ColumnDataSource(data=dict(x=x, y=y2))
+
+# Set up plotting function 
+# set up the Bokeh figure first
+plot = bk_figure(plot_height=400, plot_width=400, title="Concentration Signals (1 Dimension)",
+                 tools="crosshair,pan,reset,save,wheel_zoom",
+                 x_range=[-10, 350], y_range=[0, 15])
+plot.xaxis.axis_label = 'Horizontal Distance (m)'
+plot.yaxis.axis_label = 'Concentration (kg/m)'
+
+# input three functions with 1, 5, 10 days
+plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6, color='magenta', legend_label='5 days')
+plot.line('x', 'y', source=source1, line_width=3, line_alpha=0.6, color='blue', legend_label='10 days')
+plot.line('x', 'y', source=source2, line_width=3, line_alpha=0.6, color='green', legend_label='20 days')
 
 # Set up widgets aka. sliders and text box
-text = TextInput(title="title", value='Concentration Signals (2 Dimension)')
-vv = Slider(title="Velocity (m/day)", value=2.55, start=0.1, end=5, step=1)
-tt = Slider(title="Time (Year)", value=20, start=1, end=100, step=5)
-aa = Slider(title="Dispersion Factor", value=0.1, start=0.05, end=0.2,step=0.05)
-#bb = Slider(title="Aquifer Thickness (m)", value=1.52, start=1, end=50, step=1)
+text = TextInput(title="title", value='Concentration Signals (1 Dimension)')
+Mass = Slider(title="Pollution Mass (kg)", value=500, start=300, end=1000, step=5)
+menu = Select(options=['Well Sorted Sand', 'Glacial Till', 'Silt'], value='Well Sorted Sand', title='Soil Type')
+
+"""
+Define Bokeh event handler callback functions
+"""
 def update_title(attrname, old, new):
-    p.title.text = text.value
+    """
+    Bokeh event handler callback that changes the title of the plot
+    :param attrname: 'value' attribute of TextInput widget
+    :param old: previous title value
+    :param new: updated title value
+    :return:
+    """
+    plot.title.text = text.value
+
 
 def update_data(attrname, old, new):
+    """
+    Bokeh event handler callback that redraws the plot when the mass or menu are changed
+    :param attrname: 'value' attribute of Mass and menu widgets
+    :param old: previous values of mass and menu
+    :param new: updated values of mass and menu
+    """
     # Get the current slider values
-    v = vv.value
-    time = tt.value
-    a = aa.value
-    #b = bb.value
+    if menu.value == 'Well Sorted Sand':
+        n = 0.375
+    elif menu.value == 'Glacial Till':
+        n = 0.15
+    elif menu.value == 'Silt':
+        n = 0.42
+    M = Mass.value
+    x, y, yy, y2 = calculate_z(M, n)
+    # Generate the new curve
+    source.data = dict(x=x, y=y)
+    source1.data = dict(x=x, y=yy)
+    source2.data = dict(x=x, y=y2)
 
-    # Generate the new curve using the new dataframe
-    x, y, df = calculate_z(v, a, time)
-    source.data = dict(df)
-for w in [vv, tt, aa]:
+
+# Trigger callback to change title
+text.on_change('value', update_title)
+
+for w in [Mass, menu]:
     w.on_change('value', update_data)
 
-
 # Set up layouts and add to document
-inputs = widgetbox(text, vv, tt, aa)
-layout = row(p,widgetbox(text, vv, tt, aa))
+inputs = widgetbox(text, menu, Mass)
+layout = row(plot, column(text, menu, Mass))
 
+curdoc().add_root(layout)
+curdoc().title = "Concentration Signals"
 
-def modify_doc(doc):
-    doc.add_root(row(layout, width=800))
-    doc.title = "Sliders"
-    text.on_change('value', update_title)
-
-#show the plot and sliders 
-handler = FunctionHandler(modify_doc)
-app = Application(handler)
-show(app)
-end = timeit.timeit()
-print(end-start)
-
-
-# -math model that describes the area map of concentration 
-# -less formal 
-# -red dot at the soruce 
-# -delete the range 
-# -the groundwater flow is toward the east (right) 
-# -hot spot of concentration 
-# -explain dispersion 
 
 # In[ ]:
 
