a8b2a041b89400711cdfdbfd7c6ce7c6dda01672
Taylor Lithgow <talithgow@gmail.com>
2022-06-26 23:02:20 -0700

    update graph loading

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from app import app
+from one_dimension import script, div
 
 
 @app.route("/")
@@ -10,9 +11,13 @@ def index():
 
 @app.route("/1D")
 def one():
-    return render_template('one_dimension.html')
+    return render_template('one_dimension.html', bokeh_script=script, bokeh_div=div)
 
 
 @app.route("/2D")
 def two():
     return render_template('two_dimension.html')
+
+@app.route("/tay")
+def tay():
+    return "hello world"
