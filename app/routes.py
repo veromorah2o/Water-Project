1419692211daa3b069d22795b3a7639ccdd01460
Taylor Lithgow <talithgow@gmail.com>
2022-08-13 23:25:27 -0700

    Fix up

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from app import app
-from one_dimension import script, div
 
 
 @app.route("/")
@@ -11,12 +10,12 @@ def index():
 
 @app.route("/1D")
 def one():
-    return render_template('one_dimension.html', bokeh_script=script, bokeh_div=div)
+    return render_template('one_dimension.html')
 
 
 @app.route("/2D")
 def two():
-    return render_template('two_dimension.html', bokeh_script=script, bokeh_div=div)
+    return render_template('two_dimension.html')
 
 @app.route("/Borden")
 def borden():
